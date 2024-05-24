0e2868acbaf1c9282939adb301fa73cf3b65dbbb
Abdulazizqusay1 <153385000+Abdulazizqusay1@users.noreply.github.com>
2024-05-24 18:55:23 +0300

    Add files via upload

---
+++ AOPproject/src/main/java/com/mycompany/aopproject/AOPproject.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+package com.mycompany.aopproject;
+
+/**
+ *
+ * @author abdul
+ */
+public class AOPproject {
+
+    public static void main(String[] args) {
+
+    }
+}
